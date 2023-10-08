17c3599cf892ce3f7e819515bf3e2743abd7a27d
readthegame <126955479+readthegame@users.noreply.github.com>
2023-10-08 14:55:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,22 +12,7 @@ import streamlit as st
 st.title("Property Investment Calculator")
 
 st.header("Inputs")
-
-ca1,ca2 = st.columns(2)
-
-with ca1:
-
-    tax_nation = st.radio("Country",["UK","USA"])
-
-with ca2:
-    if tax_nation == "UK":
-
-        tax_region = st.radio("Region",["England","Wales","Scotland","N Ireland"])
-
-    else:
-
-        tax_region = st.radio("Region",["Currently not available"])
-    
+  
 with st.form(key="inputs"):
 
     c1, c2, c3 = st.columns(3)
@@ -38,9 +23,11 @@ with st.form(key="inputs"):
 
         st.subheader("Property Value")
         purchase_price = st.number_input('Purchase Price',value=100000,min_value=1)
+        purchase_tax_rate = st.number_input('Purchase Tax %',value=5,min_value=0)/100
         property_investment = st.number_input("Other Upfront investment",min_value=0)
         y1_capital_growth = st.number_input("First Year Property Value Growth %")/100
         capital_growth = st.number_input("Ongoing Annual Property Value Growth %")/100
+        sale_tax_rate = st.number_input("Sale Tax %",value=20,min_value=0,max_value=100)/100
 
     with c2:
 
@@ -49,7 +36,7 @@ with st.form(key="inputs"):
         LTV = st.number_input("LTV %",min_value=0,max_value=100,value=75)
         starting_mortgage_rate = st.number_input("Starting Mortgage Rate %",min_value=0,value=5)/100
         mortgage_term = int(st.number_input("Fixed Term (yrs)",min_value=1,max_value=25))
-        refinance_toggle = st.toggle("Refinance after Fixed Term")
+        refinance_toggle = st.toggle("Equity Release after Fixed Term")
         ongoing_mortgage_rate = st.number_input("Ongoing Mortgage Rate %",min_value=0,value=3)/100
         mortgage_fees_percentage = st.number_input("Mortgage Fees %")/100
         legal_fees_percentage = st.number_input("Legal Fees %",min_value=0,value=1)/100
@@ -59,25 +46,16 @@ with st.form(key="inputs"):
         st.subheader("Income")
         monthly_income = st.number_input("Monthly Gross Rental Income",value=500,min_value=1)            
         rental_growth = st.number_input("Annual Rental Growth %")/100
+        vacancy_rate = st.number_input("Average Vacancy Rate %",min_value=0)/100
         
         st.subheader("Costs")
         mgmt_fee_percentage = st.number_input("Management Fee %",min_value=0,value=10)/100
         other_fee_percentage = st.number_input("Other Costs %",min_value=0)/100
         inflation = st.number_input("Annual Cost Inflation %",value=2)/100
-
-        st.subheader("Tax")
-        
-        if tax_region == "England":
-        
-            tax_regime = st.selectbox("Applicable Tax Regime",["No Tax",
-                                                    "Personal Basic rate (20%)",
-                                                   "Personal Higher rate (40%)",
-                                                   "Personal Add'l rate (45%)",
-                                                   "Corporation Tax (25%)"])
-            
-        else:
-            
-            tax_regime = st.selectbox("Applicable Tax Regime",["Other"])
+        tax_rate = st.number_input("Tax Rate %",min_value=0)/100
+        tax_application = st.selectbox("Tax Rate Applied to",("Income only",
+                                                              "Income less expenses (excl mortgage costs)",
+                                                              "Income less expenses (incl mortgage costs)"))
 
     st.form_submit_button("Calculate")
 
@@ -86,6 +64,7 @@ if (purchase_price!=0)|(LTV!=0)|(monthly_income!=0):
     mortgage = LTV/100 * purchase_price
     deposit = purchase_price - mortgage
     legal_fees = purchase_price * legal_fees_percentage
+    purchase_tax = purchase_price * purchase_tax_rate
     monthly_mgmt_fee = monthly_income * mgmt_fee_percentage
     monthly_other_fee = monthly_income * other_fee_percentage
     mortgage_fees = mortgage * mortgage_fees_percentage
@@ -100,7 +79,7 @@ if (purchase_price!=0)|(LTV!=0)|(monthly_income!=0):
         refinance_amount = 0
     
     ongoing_monthly_mortgage_cost = (mortgage + refinance_amount + mortgage_fees*2) * ongoing_mortgage_rate / 12
-    monthly_pre_tax = monthly_income - monthly_mgmt_fee - monthly_other_fee - starting_monthly_mortgage_cost
+    monthly_pre_tax = monthly_income*(1-vacancy_rate) - monthly_mgmt_fee - monthly_other_fee - starting_monthly_mortgage_cost
     gross_initial_yield = (monthly_income * 12) / purchase_price
     net_initial_yield = (monthly_pre_tax * 12) / purchase_price
     ROI = (monthly_pre_tax * 12) / (deposit + legal_fees)
@@ -117,12 +96,12 @@ for i in range(0,101):
     cash_flow.at[i,"Year"] = i
 
 cash_flow["Upfront Costs"] = np.where(cash_flow["Year"]==0,
-                     -(deposit+legal_fees+property_investment),
+                     -(deposit+legal_fees+property_investment+purchase_tax),
                     0)
 
 cash_flow["Income"] = np.where(cash_flow["Year"]==0,
                               0,
-                              monthly_income*12)
+                              monthly_income*12*(1-vacancy_rate))
 
 for i in range(1,len(cash_flow)):
                
@@ -146,16 +125,24 @@ for i in range(1,len(cash_flow)):
         
         cash_flow.at[i,"Mortgage"] = -ongoing_monthly_mortgage_cost*12
 
+cash_flow["Tax"] = np.where(tax_application=="Income Only",
+                            cash_flow["Income"] * tax_rate * -1,
+                            np.where(tax_application=="Income less expenses (excl mortgage costs)",
+                                     (cash_flow["Income"] + cash_flow["Costs"]) * tax_rate * -1,
+                                     (cash_flow["Income"] + cash_flow["Costs"] + cash_flow["Mortgage"]) * tax_rate * -1))
+
 cash_flow["Refinance Income"] = np.where((refinance_toggle==True)&(cash_flow["Year"]==mortgage_term),
                                             refinance_amount,0)
 
 irr_cash_flow = cash_flow.copy()[cash_flow["Year"]<=appraisal_term]
 
 irr_cash_flow["Exit Sale Price"] = np.where(irr_cash_flow["Year"]==appraisal_term,
-                     (purchase_price * (1 + y1_capital_growth) * (1 + capital_growth) ** (appraisal_term - 1)) - mortgage - refinance_amount,
+                     ((purchase_price + property_investment) * (1 + y1_capital_growth) * (1 + capital_growth) ** (appraisal_term - 1)) - mortgage - refinance_amount,
                     0)
 
-irr_cash_flow["Total Cash Flow"] = irr_cash_flow["Upfront Costs"] + irr_cash_flow["Exit Sale Price"] + irr_cash_flow["Income"] + irr_cash_flow["Costs"] + irr_cash_flow["Mortgage"] + irr_cash_flow["Refinance Income"]
+irr_cash_flow["Exit Tax"] = irr_cash_flow["Exit Sale Price"] * sale_tax_rate * -1
+
+irr_cash_flow["Total Cash Flow"] = irr_cash_flow["Upfront Costs"] + irr_cash_flow["Exit Sale Price"] + irr_cash_flow["Income"] + irr_cash_flow["Costs"] + irr_cash_flow["Mortgage"] + irr_cash_flow["Tax"] + irr_cash_flow["Refinance Income"] irr_cash_flow["Exit Tax"]
 
 payback_cash_flow = cash_flow
 
