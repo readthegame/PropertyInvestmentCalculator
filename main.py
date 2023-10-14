cd45e7388dec872291ce0ea16d668a2c9bc846cc
readthegame <126955479+readthegame@users.noreply.github.com>
2023-10-14 19:55:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -189,29 +189,29 @@ with tab1:
       
       with col1:
           st.metric("Cash Payback (yrs)",0)
-          st.metric(str(appraisal_term)+"yr IRR",0)
+          st.metric(str(appraisal_term)+"yr IRR on Cash",0)
       with col2:
           st.metric("Net Initial Yield",0)
           st.metric("Gross Initial Yield",0)
       with col3:
-          st.metric("Capital Return",0)
-          st.metric("Income Return",0)
-          st.metric("Total Return",0)
+          st.metric("Capital Return on Cash",0)
+          st.metric("Income Return on Cash",0)
+          st.metric("Total Return on Cash",0)
   else:
       with col1:
           st.metric("Cash Payback (yrs)",payback)
-          st.metric(str(appraisal_term)+"yr IRR",str(round(irr*100,1))+"%")
+          st.metric(str(appraisal_term)+"yr IRR on Cash",str(round(irr*100,1))+"%")
       with col2:
           st.metric("Net Initial Yield",str(round(net_initial_yield*100,1))+"%")
           st.metric("Gross Initial Yield",str(round(gross_initial_yield*100,1))+"%")
       with col3:
-          st.metric("Capital Return",str(round(capital_return,1))+"%")
-          st.metric("Income Return",str(round(income_return,1))+"%")
-          st.metric("Total Return",str(round(total_return,1))+"%")
+          st.metric("Capital Return on Cash",str(round(capital_return,1))+"%")
+          st.metric("Income Return on Cash",str(round(income_return,1))+"%")
+          st.metric("Total Return on Cash",str(round(total_return,1))+"%")
   
   st.header(str(appraisal_term) + " Year Cash Flows")
   
-  irr_cash_flow
+  st.dataframe(irr_cash_flow, hide_index=True)
 
 with tab2:
 
@@ -221,15 +221,15 @@ with tab2:
 
   monte_carlo_df = pd.DataFrame(
     [
-      {"Metric":"Other Upfront Investment", "Simuate?":True, "Base": property_investment, "Min Value": max(property_investment - 5000.0,0.0), "Max Value": property_investment+5000.0},
-      {"Metric":"First Year Property Value Growth %", "Simuate?":True, "Base": y1_capital_growth*100, "Min Value": y1_capital_growth*100 - 1.0, "Max Value": y1_capital_growth*100 + 1.0},
-      {"Metric":"Ongoing Annual Property Value Growth %", "Simuate?":True, "Base": capital_growth*100, "Min Value": capital_growth*100 - 1.0, "Max Value": capital_growth*100 + 1.0},
-      {"Metric":"Ongoing Mortgage Rate %", "Simuate?":True, "Base": ongoing_mortgage_rate*100, "Min Value": max(ongoing_mortgage_rate*100 - 1.0,0.0), "Max Value": ongoing_mortgage_rate*100 + 1.0},
-      {"Metric":"Monthly Gross Rental Income", "Simuate?":True, "Base": monthly_income, "Min Value": max(monthly_income - 100.0,0), "Max Value": monthly_income + 100.0},
-      {"Metric":"Annual Rental Growth %", "Simuate?":True, "Base": rental_growth*100, "Min Value": rental_growth*100 -1.0, "Max Value": rental_growth*100 + 1.0},
-      {"Metric":"Average Vacancy Rate %", "Simuate?":True, "Base": vacancy_rate*100, "Min Value": max(vacancy_rate*100 - 1.0,0.0), "Max Value": vacancy_rate*100 +1.0},
-      {"Metric":"Other Costs %", "Simuate?":True, "Base": other_fee_percentage*100, "Min Value": max(other_fee_percentage*100 - 1.0,0.0), "Max Value": other_fee_percentage*100 - 1.0},
-      {"Metric":"Annual Cost Inflation %", "Simuate?":True, "Base": inflation*100, "Min Value": inflation*100 - 1.0, "Max Value": inflation * 100 + 1.0},
+      {"Metric":"Other Upfront Investment", "Simulate?":True, "Base": property_investment, "Min Value": max(property_investment - 5000.0,0.0), "Max Value": property_investment+5000.0},
+      {"Metric":"First Year Property Value Growth %", "Simulate?":True, "Base": y1_capital_growth*100, "Min Value": y1_capital_growth*100 - 1.0, "Max Value": y1_capital_growth*100 + 1.0},
+      {"Metric":"Ongoing Annual Property Value Growth %", "Simulate?":True, "Base": capital_growth*100, "Min Value": capital_growth*100 - 1.0, "Max Value": capital_growth*100 + 1.0},
+      {"Metric":"Ongoing Mortgage Rate %", "Simulate?":True, "Base": ongoing_mortgage_rate*100, "Min Value": max(ongoing_mortgage_rate*100 - 1.0,0.0), "Max Value": ongoing_mortgage_rate*100 + 1.0},
+      {"Metric":"Monthly Gross Rental Income", "Simulate?":True, "Base": monthly_income, "Min Value": max(monthly_income - 100.0,0), "Max Value": monthly_income + 100.0},
+      {"Metric":"Annual Rental Growth %", "Simulate?":True, "Base": rental_growth*100, "Min Value": rental_growth*100 -1.0, "Max Value": rental_growth*100 + 1.0},
+      {"Metric":"Average Vacancy Rate %", "Simulate?":True, "Base": vacancy_rate*100, "Min Value": max(vacancy_rate*100 - 1.0,0.0), "Max Value": vacancy_rate*100 +1.0},
+      {"Metric":"Other Costs %", "Simulate?":True, "Base": other_fee_percentage*100, "Min Value": max(other_fee_percentage*100 - 1.0,0.0), "Max Value": other_fee_percentage*100 + 1.0},
+      {"Metric":"Annual Cost Inflation %", "Simulate?":True, "Base": inflation*100, "Min Value": inflation*100 - 1.0, "Max Value": inflation * 100 + 1.0},
     ]
   )
 
@@ -237,7 +237,7 @@ adj_monte_carlo_df = st.data_editor(
   monte_carlo_df,
   column_config={
     "Metric":"Metric",
-    "Simulate?":st.column_config.CheckboxColumn("Simulate?",help="Select whether to include in simulation",default=True),
+    "Simulate?":st.column_config.CheckboxColumn("Simulate?",help="Select whether to include in simulation - unticking means only the Base value will be used when running the simulations",default=True),
     "Base":"Base",
     "Min Value":st.column_config.NumberColumn("Min Value",help="Enter minimum value for simulation (must be less than Max Value and no greater than Base)"),
     "Max Value":st.column_config.NumberColumn("Max Value",help="Enter maximum value for simulation (must be greater than Min Value and no less than Base)"),
@@ -245,4 +245,58 @@ adj_monte_carlo_df = st.data_editor(
   disabled=["Metric","Base"],
   hide_index=True
 )
+
+property_investment_copy = property_investment
+y1_capital_growth_copy = y1_capital_growth
+capital_growth_copy = capital_growth    
+ongoing_mortgage_rate_copy = ongoing_mortgage_rate
+monthly_income_copy = monthly_income            
+rental_growth_copy = rental_growth
+vacancy_rate_copy = vacancy_rate
+other_fee_percentage_copy = other_fee_percentage
+inflation_copy = inflation
+
+def rand_number(x):
+
+  df_temp = np.random.triangular(left=adj_monte_carlo_df.at[x,"Min Value"] * adj_monte_carlo_df.at[x,"Simulate?"] + (1 - adj_monte_carlo_df.at[x,"Simulate?"]) * adj_monte_carlo_df.at[x,"Base"],
+                                 mode= adj_monte_carlo_df.at[x,"Base"],
+                                 right= adj_monte_carlo_df.at[x,"Max Value"] * adj_monte_carlo_df.at[x,"Simulate?"] + (1 - adj_monte_carlo_df.at[x,"Simulate?"]) * adj_monte_carlo_df.at[x,"Base"],
+                                 size=100000)
+
+  return df_temp
+
+property_investment_rand = rand_number(0)
+y1_capital_growth_rand = rand_number(1)
+capital_growth_rand = rand_number(2)  
+ongoing_mortgage_rate_rand = rand_number(3)
+monthly_income_rand = rand_number(4)            
+rental_growth_rand = rand_number(5)
+vacancy_rate_rand = rand_number(6)
+other_fee_percentage_rand = rand_number(7)
+inflation_rand = rand_number(8)
+
+df_mcs = pd.DataFrame({
+    
+    "Other Upfront Investment":property_investment_rand,
+   "First Year Property Value Growth %":y1_capital_growth_rand,
+   "Ongoing Annual Property Value Growth %":capital_growth_rand,
+   "Ongoing Mortgage Rate %":ongoing_mortgage_rate_rand,
+   "Monthly Gross Rental Income":monthly_income_rand,
+   "Annual Rental Growth %":rental_growth_rand,
+   "Average Vacancy Rate %":vacancy_rate_rand,
+   "Other Costs %":other_fee_percentage_rand,
+   "Annual Cost Inflation %":inflation_rand
+    
+})
+
+#now add in outputs from calc
+
+for i in range(
+
+with st.form(key="mcs_inputs"):
+
+  col1, col2 = st.columns(2)
+
+with col1:
+  
 
